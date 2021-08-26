b75bfe3083fbd1567eccb40cf54def3be818616d
ottomossei <seki.jobhunting@gmail.com>
2021-08-27 00:05:17 +0900

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name="pycheck",
-    version="1.0.2",
+    version="1.0.3",
     description="A Python tool that mainly supports unit testing.",
     author="ottomossei",
     author_email="seki.jobhunting@gmail.com",
